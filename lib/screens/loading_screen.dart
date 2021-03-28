5bce73cd19e14585da382ba0e3c836e000101a30
danhausen <danihausen@live.com>
2021-03-28 10:58:08 -0300

    main

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,6 +7,18 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    print('initState called');
+  }
+
+  void deactivate() {
+    super.deactivate();
+    print('deactivate called');
+  }
+
   void getLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
