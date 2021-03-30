69d61958f1610360977089b64816240b8fba588b
danhausen <danihausen@live.com>
2021-03-30 18:08:18 -0300

    main

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -26,6 +27,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   void somethingThatExpectsLessThan10(int n) {
     //this is a throw example
     if (n > 10) {
@@ -35,6 +46,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
