2c8417e81069f4182a23464774ba167e2ac6fc62
danhausen <danihausen@live.com>
2021-04-01 16:50:31 -0300

    main

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:weather/services/location.dart';
 import 'package:weather/services/networking.dart';
+import 'package:weather/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const weatherApiKey = 'e708230e83bde6b84165ad462b7a9fc0';
 
@@ -35,6 +37,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
             'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$weatherApiKey');
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   void somethingThatExpectsLessThan10(int n) {
@@ -46,6 +52,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitCircle(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
